eae3b8f1780c33df3ce7216bf576cf272bc6eb03
hemantkumar2323 <freelanceb23@gmail.com>
2025-05-09 23:38:05 +0530

    Update trends.js

--- api/trends.js
+++ api/trends.js
@@ -1,1 +1,20 @@
+import { getTikTokTrends, getRedditPsychTrends } from "../utils/scraper.js";
+import { scoreContent } from "../utils/scorer.js";
+
+export default async function handler(req, res) {
+  const allTrends = [];
+
+  const sources = [
+    await getTikTokTrends(),
+    await getRedditPsychTrends(),
+  ];
+
+  sources.flat().forEach((item) => {
+    if (scoreContent(item)) {
+      allTrends.push(item);
+    }
+  });
+
+  res.status(200).json({ results: allTrends });
+}
 
